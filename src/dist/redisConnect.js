e4cebfcb3c859ac7ebffea2c41109497fc3f4dbb
hellboykill <badboy1998hh@gmail.com>
2021-11-12 10:05:59 +0700

    rm build

--- src/dist/redisConnect.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.clientRedis = void 0;
-var redis = require("redis");
-exports.clientRedis = redis.createClient({
-    host: '192.168.1.187',
-    port: 6379
-});
